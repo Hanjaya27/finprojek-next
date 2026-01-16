1c83acb5aa0c38c833112754321aa473ddab6caf
bilqis salma <bilqissalma@MacBook-Air-bilqis.local>
2026-01-16 18:00:52 +0700

    fix: use env API url for production

--- lib/axios.ts
+++ lib/axios.ts
@@ -1,16 +1,18 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://api.finprojek.web.id/api",
+  baseURL: process.env.NEXT_PUBLIC_API_URL,
   headers: {
     Accept: "application/json",
   },
 });
 
 api.interceptors.request.use((config) => {
-  const token = localStorage.getItem("auth_token");
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
+  if (typeof window !== "undefined") {
+    const token = localStorage.getItem("auth_token");
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
   }
   return config;
 });
